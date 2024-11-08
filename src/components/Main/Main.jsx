bcbc725c7852c3761dc0c7bb8325ce1de9881bc4
tatyanepgoncalves <tatyane.2324165@aluno.faculdadedescomplica.com.br>
2024-11-08 16:06:55 -0300

    ✨ feat: Componente Main

---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,19 @@
+import Home from "./Home";
+import Habilidades from "./Habilidades";
+import Projetos from "./Projetos";
+import About from "./About";
+import Contact from "./Contact";
+
+export default function Main() {
+  return (
+    <>
+    <main>
+      <Home />
+      <About />
+      <Habilidades />
+      <Projetos />
+      <Contact />
+    </main>
+    </>
+  )
+}
