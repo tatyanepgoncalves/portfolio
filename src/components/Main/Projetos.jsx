4405e93a1e1e5b269392f7aed3282682d47f3120
tatyanepgoncalves <tatyane.2324165@aluno.faculdadedescomplica.com.br>
2024-11-08 16:07:30 -0300

    ✨ feat: Componente Projetos

---
+++ src/components/Main/Projetos.jsx
@@ -0,0 +1,12 @@
+import ProjectList from "./ProjetoList"
+
+export default function Projetos() {
+  return (
+    <section id="projetos">
+      <h2>Projetos</h2>
+      
+      <ProjectList />
+     
+    </section>
+  );
+}
