1e86fd7ed46a5dcb11b88c1ff718178460f72e36
tatyanepgoncalves <tatyane.2324165@aluno.faculdadedescomplica.com.br>
2024-11-08 16:05:13 -0300

    ✨ feat: Componente Button Call to Action

---
+++ src/components/Buttons/BtnCallToAction.jsx
@@ -0,0 +1,10 @@
+import { ArrowDownRight } from "lucide-react" 
+
+export default function BtnCallToAction() {
+  return (
+    <a href="#projetos" className="btn-home">
+      Veja projetos
+      <ArrowDownRight size={24} />
+    </a>
+  )
+}
