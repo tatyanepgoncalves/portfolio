c791b06060f3c56291e9ad4bef9582a4c2860b08
tatyanepgoncalves <tatyane.2324165@aluno.faculdadedescomplica.com.br>
2024-11-08 16:06:43 -0300

    ✨ feat: Componente Home

---
+++ src/components/Main/Home.jsx
@@ -0,0 +1,18 @@
+import SocialMedia from "../SocialMedia/SocialMedia";
+import BtnCallToAction from "../Buttons/BtnCallToAction";
+import "../../home.scss";
+
+export default function Home() {
+  return (
+    <section id="home">
+      <div className="title">
+        <h1>Frase que promova</h1>
+        <span>entrega de valor</span>
+      </div>
+      
+      <p>Seu nome e uma breve descrição profissional sobre você, habilidades e o que faz.</p>
+      <SocialMedia />
+      <BtnCallToAction />
+    </section>
+  )
+}
