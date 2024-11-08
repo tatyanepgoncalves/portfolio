a5ddb41527d455f81a3f348e712f21aae2b7ee18
tatyanepgoncalves <tatyane.2324165@aluno.faculdadedescomplica.com.br>
2024-11-08 16:05:23 -0300

    ✨ feat: Componente Footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,15 @@
+import { Copyright } from "lucide-react";
+
+export default function Footer() {
+  return (
+    <footer>
+      <div className="text">
+        <Copyright size={24} />
+        <p>
+          2024. Todos os Direitos Reservados. Tatyane Gonçalves. Desenvolvido por
+          TG.
+        </p>
+      </div>
+    </footer>
+  );
+}
