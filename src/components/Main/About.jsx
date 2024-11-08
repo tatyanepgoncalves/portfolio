0858e7dd70cfc94aa889e43a7e1fd141fca7786f
tatyanepgoncalves <tatyane.2324165@aluno.faculdadedescomplica.com.br>
2024-11-08 16:05:44 -0300

    ✨ feat: Componente About

---
+++ src/components/Main/About.jsx
@@ -0,0 +1,38 @@
+import imageAvatar from "../../images/Tatyane.jpg";
+import { Languages } from "lucide-react";
+import Education from "./Education";
+
+export default function About() {
+  return (
+    <section id="about">
+      <article className="about-top">
+        <div className="box-text">
+          <h2>Sobre mim</h2>
+          <p>Descrição sobre você e suas habilidades e experiências. Descrição sobre você e suas habilidades e experiências. Descrição sobre você e suas habilidades e experiências. Descrição sobre você e suas habilidades e experiências. Descrição sobre você e suas habilidades e experiências. Descrição sobre você e suas habilidades e experiências. Descrição sobre você e suas habilidades e experiências. Descrição sobre você e suas habilidades e experiências. </p>
+
+          <div className="language-box">
+            <Languages size={24} />
+            <div className="linguage-item">
+              <h3>Espanhol</h3>
+              <p>Intermediário</p>
+            </div>
+            <div className="linguage-item">
+              <h3>Inglês </h3>
+              <p>Básico</p>
+            </div>
+          </div>
+        </div>
+        
+        <div className="box-image">
+          <img src={imageAvatar} alt="Tatyane Gonçalves" />
+        </div>
+
+
+      </article>
+
+      <Education />
+
+     
+    </section>
+  )
+}
