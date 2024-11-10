1dfa5bf3862eeecbb26505e0b37c118e70f99ed6
tatyanepgoncalves <tatyane.2324165@aluno.faculdadedescomplica.com.br>
2024-11-10 16:46:08 -0300

    ✨ feat: Componente Contact.jsx

--- src/components/Main/Contact.jsx
+++ src/components/Main/Contact.jsx
@@ -1,4 +1,6 @@
 import SocialMedia from "../SocialMedia/SocialMedia";
+import { MdEmail } from "react-icons/md";
+import "../../styles/_contact.scss";
 
 export default function Contact() {
   return (
@@ -7,7 +9,11 @@ export default function Contact() {
       <p>Estou disponível para serviços e novas conexões. Contate-me via e-mail e conecte-se comigo através das minhas redes sociais.</p>
       <SocialMedia />
 
-      <a href="mailto:tatypg023@gmail.com" target="_blank" className="email">tatypg023@gmail.com</a>
+      <div className="box-email">
+        <MdEmail />
+        <a href="mailto:tatypg023@gmail.com" target="_blank" className="email">tatypg023@gmail.com</a>
+      </div>
+
     </section>
   )
 }
