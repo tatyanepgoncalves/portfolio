b5929b78721d476722f7cb1b3e3f1da552dc84a5
tatyanepgoncalves <tatyane.2324165@aluno.faculdadedescomplica.com.br>
2024-11-10 16:46:21 -0300

    ✨ feat: Componente Education.jsx

--- src/components/Main/Education.jsx
+++ src/components/Main/Education.jsx
@@ -1,4 +1,5 @@
-import Curses from "./Curses"
+import Curses from "./Curses";
+import { University, GraduationCap } from "lucide-react";
 
 export default function Education() {
   return (
@@ -7,8 +8,17 @@ export default function Education() {
         <h2>Formação</h2>
         <div className="education-box">
           <h3>Bacharel em Sistemas de Informação</h3>
-          <span>Abr / 2023 - Mar / 2027</span>
-          <p>Descomplica Faculdade</p>
+          <span>Abr/2023 - Mar/2027</span>
+
+          <div className="instituicao">
+            <University />
+            <p>Descomplica Faculdade</p>
+          </div>
+
+          <div className="title">
+            <GraduationCap />
+            <p>Graduação</p>
+          </div>
         </div>
       </div>
 
