c3b141d0d533145d490c87340308c828af51edba
tatyanepgoncalves <tatyane.2324165@aluno.faculdadedescomplica.com.br>
2024-11-10 18:18:34 -0300

    ✨ feat: Add Links no Componente  SocialMedia.jsx

--- src/components/SocialMedia/SocialMedia.jsx
+++ src/components/SocialMedia/SocialMedia.jsx
@@ -4,9 +4,9 @@ import { Instagram, Github, Linkedin } from "lucide-react";
 export default function SocialMedia() {
   return (
     <div className="social-media"> 
-      <a href=""><Github  size={24}/></a>
-      <a href=""><Linkedin size={24} /></a>
-      <a href=""><Instagram  size={24}/></a>
+      <a href="https://github.com/tatyanepgoncalves" target="_blank"><Github  size={24}/></a>
+      <a href="https://www.linkedin.com/in/tatyanegoncalves/" target="_blank"><Linkedin size={24} /></a>
+      <a href="https://www.instagram.com/devtatyane/" target="_blank"><Instagram  size={24}/></a>
     </div>
   )
 }
