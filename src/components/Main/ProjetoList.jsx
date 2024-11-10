237749ea8f489bb236540fc637cbc2ba3d8576a4
tatyanepgoncalves <tatyane.2324165@aluno.faculdadedescomplica.com.br>
2024-11-10 16:46:50 -0300

    ✨ feat: Componente ProjetoList.jsx

--- src/components/Main/ProjetoList.jsx
+++ src/components/Main/ProjetoList.jsx
@@ -1,53 +1,65 @@
 import { ArrowUpRight } from "lucide-react";
 
-
 const projectData = [
   {
     id: 1,
-    image: "../../images/projeto-detona-ralph.png",
-    title:  "Jogo do Detona Ralph",
-    description: "Jogo baseado no filme Detona Ralph, aonde o objetivo é acertar o Ralph na janela que ele aparecer. Projeto do bootcamp da DIO em parceria com a Ri Happy.",
-    techs: ["HTML", "CSS", "JavaScript"],
+    image: "Projeto 1",
+    title: "Nome do Projeto",
+    description: "Breve descrição sobre o projeto",
+    techs: ["Tech 1", "Tech 2", "Tech 3"],
     code: "https://github.com/tatyanepgoncalves/Jogo-Detona-Ralph/tree/main",
-    deploy: "https://tatyanepgoncalves.github.io/Jogo-Detona-Ralph/"
+    deploy: "https://tatyanepgoncalves.github.io/Jogo-Detona-Ralph/",
   },
   {
     id: 2,
-    image: "../../images/Projeto-Mundo-Normal.png",
-    title: "Landing Page Stranger Things",
-    description: "Landing Page desenvolvida baseado na série Stranger Things usando HTML, SCSS e JavaScript no bootcamp da DIO.",
-    techs: ["HTML", "SCSS", "JavaScript"],
+    image: "Projeto 2",
+    title: "Nome do Projeto",
+    description: "Breve descrição sobre o projeto",
+    techs: ["Tech 1", "Tech 2", "Tech 3"],
     code: "https://github.com/tatyanepgoncalves/Website-stranger-things",
-    deploy: "https://website-stranger-things.vercel.app/"
+    deploy: "https://website-stranger-things.vercel.app/",
   },
   {
     id: 3,
-    image: "../../images/projeto-bookmark.png",
-    title: "Bookmark landing page",
-    description: "Landind Page de página de destino aos favoritos",
-    techs: ["HTML", "SCSS", "React", "Vite"],
+    image: "Projeto 3",
+    title: "Nome do Projeto",
+    description: "Breve descrição sobre o projeto",
+    techs: ["Tech 1", "Tech 2", "Tech 3", "Tech 4"],
     code: "https://github.com/tatyanepgoncalves/Bookmark-website",
-    deploy: "https://tatyanepgoncalves.github.io/Bookmark-website/"
-  }
+    deploy: "https://tatyanepgoncalves.github.io/Bookmark-website/",
+  },
 ];
 
 export default function ProjetoList() {
-
   return (
     <article className="project-list">
       {projectData.map((projeto) => (
-        <div key={projeto.id}>
-          <img src={projeto.image} alt={projeto.title} />
+        <div key={projeto.id} className="project-item">
+          <div className="image-box">
+            <p>{projeto.image}</p>
+          </div>
           <h3>{projeto.title}</h3>
           <p>{projeto.description}</p>
           <ul className="techs-used">
             {projeto.techs.map((tech, index) => (
-              <li key={index}>{tech}</li>
+              <li key={index}><p>{tech}</p></li>
             ))}
           </ul>
 
-         <a href={projeto.code} target="_blank" > Ver repositório</a>
-         <a href={projeto.deploy} target="_blank">Ver projeto <ArrowUpRight size={24} /></a>
+          <div className="links-project">
+            <div className="box-link">
+              <a href={projeto.deploy} target="_blank">
+                Prévia do projeto
+              </a>
+              <ArrowUpRight size={24} />
+            </div>
+            <div className="box-link">
+              <a href={projeto.code} target="_blank">
+                
+                Repositório
+              </a>
+            </div>
+          </div>
         </div>
       ))}
     </article>
